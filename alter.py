f77babb4135d752351f8496f331e3a435d9af81b
busybean3 <dbwo4011@gmail.com>
2022-05-27 21:45:23 +0900

    Update alter.py

--- alter.py
+++ alter.py
@@ -4,5 +4,6 @@ import requests
 #-d "{\"text\":\"빨리 끝내고 자자. :rocket:\"}" \
 #https://hooks.slack.com/services/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
 def alert(message):
-    r = requests.post('https://hooks.slack.com/services/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', json={"text": "{}:red_circle:".format(message)}, headers={"Content-Type": "application/json"})
+    r = requests.post('https://hooks.slack.com/services/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', json={"text": "{}:red_circle:".format(message)}, headers={"Content-Type": "application/json"})
+    print(r.content)
     return True
